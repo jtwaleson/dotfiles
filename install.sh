fc2e78a8f245c36cbaf49453a17c9d91e5677d6c
Jouke Waleson <jouke@waleson.com>
2022-04-01 17:51:29 +0200

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,95 @@
 
 set -euxo pipefail
 
-ln -sf ~/.vimrc ~/dev/dotfiles/vimrc
-ln -sf ~/.zshrc ~/dev/dotfiles/zshrc
-ln -sf ~/.config/i3/config ~/dev/dotfiles/i3config
+sudo apt update
+sudo apt install \
+    apt-transport-https \
+    bzip2 \
+    ca-certificates \
+    coreutils \
+    cron \
+    curl \
+    default-mysql-client \
+    dnsutils \
+    dos2unix \
+    feh \
+    ffmpeg \
+    file \
+    g++ \
+    gcc \
+    gimp \
+    git \
+    git-lfs \
+    gnome-screenshot \
+    gnupg \
+    gparted \
+    gzip \
+    hdparm \
+    i3 \
+    imagemagick \
+    jq \
+    keepassx \
+    lsb-release \
+    moreutils \
+    mtr \
+    nmap \
+    nmon \
+    ntp \
+    openssh-client \
+    openssh-server \
+    pavucontrol \
+    postgresql-client \
+    powertop \
+    procps \
+    pwgen \
+    python-is-python3 \
+    python3-dev \
+    python3-pip \
+    python3-venv \
+    reptyr \
+    rsync \
+    silversearcher-ag \
+    sshfs \
+    strace \
+    telnet \
+    texlive \
+    texlive-latex-extra \
+    texlive-xetex \
+    tig \
+    traceroute \
+    tree \
+    util-linux \
+    v4l-utils \
+    vim \
+    vim-tiny \
+    vlc \
+    whois \
+    xclip \
+    zsh \
+    nmon # marker
+
+pip3 install requests pytest
+
+#@rm ~/.vimrc ~/.zshrc ~/.config/i3/config
+
+ln -sf ~/dev/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dev/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dev/dotfiles/i3config ~/.config/i3/config
+
+
+sudo apt install \
+    code \
+    google-chrome-stable \
+    spotify-client \
+    syncthing \
+    nmon # marker
+
+
+
+
+
+# manually install:
+# google chrome
+# VS Code
+# docker
+# spotify
